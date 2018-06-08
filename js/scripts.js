f5cc815ebb914de37057f3bb633fc5e5c44fdb3d
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-06-08 10:25:48 -0700

    add scripts to business logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,22 @@ var beepBoop = function(userInput){
   if (userInput <1 || isNaN(userInput)){
     return "INVALID ENTRY"
   }
+  else {
+    for (var i = 0; i <= userInput; i++){
+      if(i % 10 === 0){
+        arr.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      }
+      else if(i.toString().match(1)){
+        arr.push("Boop!")
+      }
+      else if(i.toString().match(0)){
+        arr.push("Beep!")
+      } else
+      arr.push(i)
+    }
+  }
+  return arr;
 }
-
 
 
 // user interface logic
