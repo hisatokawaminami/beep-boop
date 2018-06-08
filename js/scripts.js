afa232c31fab7b3d822a8121ca9a06abd7b22b13
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-06-08 14:47:40 -0700

    add comments in scripts, add background color

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,35 @@ var beepBoop = function(userInput){
   if (userInput <1 || isNaN(userInput)){
     return "INVALID ENTRY"
   }
+  //to make starting 0 -> Beep
   else {
     for (var i = 0; i <= userInput; i++){
       if(i === 0){
         arr.push("Beep!")
       }
+      // divisible by 3 -> Sorry Dave
       else if(i % 3 === 0){
         arr.push("I'm sorry, Dave. I'm afraid I can't do that.")
+
+      // 1s -> Boop
       } else if(ruleOne(i)=== true){
-          arr.push("Boop!")
+        arr.push("Boop!")
 
-console.log(arr)
+        console.log(arr)
 
-        } else if(ruleZero(i)=== true){
-            arr.push("Beep!")
-          } else
-              arr.push(i)
-        }
-      }
-    return arr;
+      // 0s -> Beep
+      } else if(ruleZero(i)=== true){
+        arr.push("Beep!")
+
+      // else shows as numbers
+      } else
+      arr.push(i)
+    }
   }
+  return arr;
+}
 
+// to find 1s
 var ruleOne  = function(userInput){
   var result = userInput.toString().indexOf(1);
   if(result === -1){
@@ -33,7 +41,7 @@ var ruleOne  = function(userInput){
     return true;
   }
 }
-
+// to find 0s
 var ruleZero = function(userInput){
   var result = userInput.toString().indexOf(0);
   if(result === -1){
@@ -46,10 +54,10 @@ var ruleZero = function(userInput){
 
 // user interface logic
 $(document).ready(function() {
- $("form#formOne").submit(function(event) {
-   event.preventDefault();
-   var userInput = parseInt($("input#userInput").val());
-   var output = beepBoop(userInput);
-   $("#result").text(output);
- });
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#userInput").val());
+    var output = beepBoop(userInput);
+    $("#result").text(output);
+  });
 });
